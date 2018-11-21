d1fc1d8ba83c7494b03d8c989e0d952c705e653d
oran rozenbaum <oran.rozenbaum@gmail.com>
2018-11-21 20:04:12 +0200

    last modified

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,242 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
 
+
+let counter = 1;
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
+
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        /*  var temp = JSON.stringify(parsedCode);*/
+
+        counter = 1;
+        parser(parsedCode);
+        show();
+        JSON.stringify(parsedCode);
     });
 });
+
+
+let table = [];
+//let tempName = '';
+
+function parser(parsedCode){
+    if (parsedCode.type != null) {
+        let f = dictionary[parsedCode.type](parsedCode);
+        return f;
+    }
+
+
+
+}
+
+function parsProgram(code) {
+
+    if (code.body != null)
+    {
+        var i;
+        //table.push({ Line: counter, Type: 'function declaration', Name: 'binarySearch' ,Condition:'', Value: ''});
+        for( i=0;i<code.body.length;i++){
+            parser(code.body[i]);
+        }
+    }
+
+}
+
+function parsFunctionDeclaration(code) {
+    if(code.id != null)
+    {
+        parser(code.id);
+    }
+    if(code.params != null)
+    {
+        var i;
+        for( i=0;i<code.params.length;i++){
+            table.push({ Line: counter, Type: code.type, Name: code.params[i].name ,Condition:'', Value: ''});
+        }
+        counter++;
+        if(code.body != null)
+        {
+            parser(code.body);
+        }
+    }
+
+}
+
+function parsIdentifier(code) {
+    if(code.type == 'Identifier'){
+        return code.name;
+    }
+}
+
+function parsBlockStatement(code) {
+    if (code.body != null)
+    {
+        var i;
+        //table.push({ Line: counter, Type: 'function declaration', Name: 'binarySearch' ,Condition:'', Value: ''});
+        for( i=0;i<code.body.length;i++){
+            parser(code.body[i]);
+        }
+    }
+
+}
+
+function parsVariableDeclaration(code) {
+
+    if(code.declarations != null)
+    {
+        var i;
+        for(i=0;i<code.declarations.length;i++){
+            let name = parser(code.declarations[i].id);
+            let val = code.declarations[i].init;
+            table.push({ Line: counter, Type: code.type , Name:name ,Condition:'', Value: val});
+        }
+        counter++;
+    }
+
+}
+
+function parsExpressionStatement(code) {
+    parser(code.expression);
+}
+
+function parsWhileStatement(code) {
+    let cond = parser(code.test);
+    table.push({ Line: counter, Type: code.type , Name: '' ,Condition: cond, Value: ''});
+    counter++;
+    parser(code.body);
+
+}
+
+function parsAssignmentExpression(code) {
+    let name = parser(code.left);
+    let val = parser(code.right);
+    table.push({ Line: counter, Type: code.type , Name: name ,Condition:'', Value: val});
+    counter++;
+
+}
+
+function parsBinaryExpression(code) {
+    let left = parser(code.left);
+    let right = parser(code.right);
+    return left +' '+code.operator + ' '+  right;
+
+}
+
+function parsLiteral(code) {
+    return code.value;
+
+}
+
+function parsIfStatement(code) {
+    let cond = parser(code.test);
+    table.push({ Line: counter, Type: code.type , Name: '' ,Condition: cond, Value: ''});
+    counter++;
+    if(code.consequent != null){
+        parser(code.consequent);
+    }
+
+    if(code.alternate != null)
+    {
+        parser(code.alternate);
+    }
+
+}
+
+function parsReturnStatement(code) {
+    let val = parser(code.argument);
+    table.push({ Line: counter, Type: code.type , Name: '' ,Condition: '', Value: val});
+    counter++;
+}
+
+function parsUnaryExpression(code) {
+    let val = parser(code.argument);
+    if(code.prefix == true)
+    {
+        return (code.operator + val);
+    }
+    else{
+        return(val+code.operator);
+    }
+}
+
+function parsMemberExpression(code) {
+    let ob = parser(code.object);
+    let pro = parser(code.property);
+    return (ob + '[' + pro + ']');
+}
+
+function parsForStatement(code){
+    let init = parser(code.init);
+    let test = parser(code.test);
+    let up = parser(code.update);
+    let con = init + ';' + test + ';' + up;
+    table.push({ Line: counter, Type: code.type , Name: '' ,Condition: con, Value: ''});
+    counter++;
+    parser(code.body);
+
+
+}
+function parsUpdateExpression(code) {
+    let val = parser(code.argument);
+    if(code.prefix == true)
+    {
+        return (code.operator + val);
+    }
+    else{
+        return(val+code.operator);
+    }
+
+}
+
+let dictionary = {
+    'VariableDeclaration': parsVariableDeclaration,
+    'Program' : parsProgram,
+    'FunctionDeclaration': parsFunctionDeclaration,
+    'BlockStatement': parsBlockStatement,
+    'ExpressionStatement': parsExpressionStatement,
+    'WhileStatement': parsWhileStatement,
+    'AssignmentExpression': parsAssignmentExpression,
+    'BinaryExpression': parsBinaryExpression,
+    'Identifier': parsIdentifier,
+    'Literal': parsLiteral,
+    'IfStatement':parsIfStatement,
+    'ReturnStatement': parsReturnStatement,
+    'UnaryExpression': parsUnaryExpression,
+    'MemberExpression': parsMemberExpression,
+    'ForStatement': parsForStatement,
+    'UpdateExpression': parsUpdateExpression
+
+
+};
+
+function show() {
+    let FinalTable = document.getElementById('myTable');
+    newTable(FinalTable);
+    for (let i = 0; i< table.length; i++)
+    {
+        var row = FinalTable.insertRow(i+1);
+        var c1 = row.insertCell(0);
+        var c2 = row.insertCell(1);
+        var c3 = row.insertCell(2);
+        var c4 = row.insertCell(3);
+        var c5 = row.insertCell(4);
+        c1.innerHTML = table[i].Line;
+        c2.innerHTML = table[i].Type;
+        c3.innerHTML = table[i].Name;
+        c4.innerHTML = table[i].Condition;
+        c5.innerHTML = table[i].Value;
+    }
+    table = [];
+}
+
+function newTable(table)
+{
+    var i;
+    let length = table.rows.length;
+    for(i=length-1;i>0;i--){
+        table.deleteRow(i);
+    }
+}
--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -8,11 +8,90 @@ describe('The javascript parser', () => {
             '{"type":"Program","body":[],"sourceType":"script"}'
         );
     });
-
     it('is parsing a simple variable declaration correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('let a = 1;')),
             '{"type":"Program","body":[{"type":"VariableDeclaration","declarations":[{"type":"VariableDeclarator","id":{"type":"Identifier","name":"a"},"init":{"type":"Literal","value":1,"raw":"1"}}],"kind":"let"}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a update expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('i++')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"UpdateExpression","operator":"++","argument":{"type":"Identifier","name":"i"},"prefix":false}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a update expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('++i')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"UpdateExpression","operator":"++","argument":{"type":"Identifier","name":"i"},"prefix":true}}],"sourceType":"script"}'
+        );
+    });
+
+
+    it('is parsing a returnStatement and function declaration correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('function check(i){return i;}')),
+            '{"type":"Program","body":[{"type":"FunctionDeclaration","id":{"type":"Identifier","name":"check"},"params":[{"type":"Identifier","name":"i"}],"body":{"type":"BlockStatement","body":[{"type":"ReturnStatement","argument":{"type":"Identifier","name":"i"}}]},"generator":false,"expression":false,"async":false}],"sourceType":"script"}'
+        );
+    });
+
+
+    it('is parsing a member expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('table[0] = 1;')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"MemberExpression","computed":true,"object":{"type":"Identifier","name":"table"},"property":{"type":"Literal","value":0,"raw":"0"}},"right":{"type":"Literal","value":1,"raw":"1"}}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a member expression and indentifier and literal correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('temp = table[0]')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"Identifier","name":"temp"},"right":{"type":"MemberExpression","computed":true,"object":{"type":"Identifier","name":"table"},"property":{"type":"Literal","value":0,"raw":"0"}}}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a binary expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('temp = (x+y)*2')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"Identifier","name":"temp"},"right":{"type":"BinaryExpression","operator":"*","left":{"type":"BinaryExpression","operator":"+","left":{"type":"Identifier","name":"x"},"right":{"type":"Identifier","name":"y"}},"right":{"type":"Literal","value":2,"raw":"2"}}}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a binary expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('temp = (x*y) + a[3];')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"Identifier","name":"temp"},"right":{"type":"BinaryExpression","operator":"+","left":{"type":"BinaryExpression","operator":"*","left":{"type":"Identifier","name":"x"},"right":{"type":"Identifier","name":"y"}},"right":{"type":"MemberExpression","computed":true,"object":{"type":"Identifier","name":"a"},"property":{"type":"Literal","value":3,"raw":"3"}}}}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a binary expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('x = x +2;')),
+            '{"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"Identifier","name":"x"},"right":{"type":"BinaryExpression","operator":"+","left":{"type":"Identifier","name":"x"},"right":{"type":"Literal","value":2,"raw":"2"}}}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing a while statement + binary expression correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('while(x<y){x=a[0]+1;}')),
+            '{"type":"Program","body":[{"type":"WhileStatement","test":{"type":"BinaryExpression","operator":"<","left":{"type":"Identifier","name":"x"},"right":{"type":"Identifier","name":"y"}},"body":{"type":"BlockStatement","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"Identifier","name":"x"},"right":{"type":"BinaryExpression","operator":"+","left":{"type":"MemberExpression","computed":true,"object":{"type":"Identifier","name":"a"},"property":{"type":"Literal","value":0,"raw":"0"}},"right":{"type":"Literal","value":1,"raw":"1"}}}}]}}],"sourceType":"script"}'
+        );
+    });
+
+    it('is parsing an for statement + vriable declartion correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('for(let i=0;i<10;i++){var che = (i*2);}')),
+            '{"type":"Program","body":[{"type":"ForStatement","init":{"type":"VariableDeclaration","declarations":[{"type":"VariableDeclarator","id":{"type":"Identifier","name":"i"},"init":{"type":"Literal","value":0,"raw":"0"}}],"kind":"let"},"test":{"type":"BinaryExpression","operator":"<","left":{"type":"Identifier","name":"i"},"right":{"type":"Literal","value":10,"raw":"10"}},"update":{"type":"UpdateExpression","operator":"++","argument":{"type":"Identifier","name":"i"},"prefix":false},"body":{"type":"BlockStatement","body":[{"type":"VariableDeclaration","declarations":[{"type":"VariableDeclarator","id":{"type":"Identifier","name":"che"},"init":{"type":"BinaryExpression","operator":"*","left":{"type":"Identifier","name":"i"},"right":{"type":"Literal","value":2,"raw":"2"}}}],"kind":"var"}]}}],"sourceType":"script"}'
+        );
+    });
+
+
+    it('is parsing an if statement + unary correctly', () => {
+        assert.equal(
+            JSON.stringify(parseCode('if(l>10){i = -1}')),
+            '{"type":"Program","body":[{"type":"IfStatement","test":{"type":"BinaryExpression","operator":">","left":{"type":"Identifier","name":"l"},"right":{"type":"Literal","value":10,"raw":"10"}},"consequent":{"type":"BlockStatement","body":[{"type":"ExpressionStatement","expression":{"type":"AssignmentExpression","operator":"=","left":{"type":"Identifier","name":"i"},"right":{"type":"UnaryExpression","operator":"-","argument":{"type":"Literal","value":1,"raw":"1"},"prefix":true}}}]},"alternate":null}],"sourceType":"script"}'
         );
     });
 });
